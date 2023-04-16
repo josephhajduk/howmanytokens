5591fb52d45d01bea0cc834a2c194fb8d75393fc
josephhajduk <joseph@solidys.dev>
2023-04-16 15:11:16 -0500

    blindly let gpt4 "clean it up"

--- src/main.rs
+++ src/main.rs
@@ -9,67 +9,58 @@ use tokenizers::tokenizer::{Result, Tokenizer};
 
 async fn tokenize(tokenizer: Arc<Tokenizer>, req: Request<Body>) -> Result<Response<Body>> {
     match (req.method(), req.uri().path()) {
-        (&Method::POST, str) => {
-            match str {
-                "/len" => {
+        (&Method::POST, "/len") => {
+            let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
+            let body_string = String::from_utf8(body_bytes.to_vec())?;
+            let encoded = tokenizer.encode(body_string, false)?;
+            println!("{}", encoded.len());
+            Ok(Response::new(Body::from(encoded.len().to_string())))
+        }
+        (&Method::POST, path) if path.starts_with("/trim/") => {
+            let len = path.strip_prefix("/trim/").unwrap().parse::<usize>().ok();
+            match len {
+                Some(len) => {
                     let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
                     let body_string = String::from_utf8(body_bytes.to_vec())?;
                     let encoded = tokenizer.encode(body_string, false)?;
-                    println!("{}",encoded.len());
-                    Ok(Response::new(Body::from(encoded.len().to_string())))
+                    let mut trimmed = encoded.get_ids().to_vec();
+                    trimmed.truncate(len);
+                    let decoded = tokenizer.decode(trimmed, true).unwrap();
+                    Ok(Response::new(Body::from(decoded)))
                 }
-                s if s.starts_with("/trim/") => {
-                    match s.strip_prefix("/trim/").unwrap().parse::<usize>() {
-                        Ok(len) => {
-                            let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
-                            let body_string = String::from_utf8(body_bytes.to_vec())?;
-                            let encoded = tokenizer.encode(body_string, false)?;
-                            let mut trimmed = encoded.get_ids().to_vec();
-                            trimmed.truncate(len);
-                            let decoded = tokenizer.decode(trimmed,true).unwrap();
-                            println!("Trimmed to {}",len);
-                            Ok(Response::new(Body::from(decoded)))}
-                        Err(_) => {
-                            let mut not_found = Response::default();
-                            *not_found.status_mut() = StatusCode::NOT_FOUND;
-                            Ok(not_found)}
-                    }
-                }
-                s if s.starts_with("/trimw/") => {
-                    match s.strip_prefix("/trimw/").unwrap().parse::<usize>() {
-                        Ok(len) => {
-                            let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
-                            let body_string = String::from_utf8(body_bytes.to_vec())?;
-                            let encoded = tokenizer.encode(body_string, false)?;
-                            let mut trimmed = encoded.get_ids().to_vec();
-                            trimmed.truncate(len);
-                            let decoded = tokenizer.decode(trimmed, true).unwrap();
-                            match decoded.rsplit_once(" ")  {
-                                None => {
-                                    println!("Trimmed word to {}", decoded.len());
-                                    Ok(Response::new(Body::from(decoded)))
-                                }
-                                Some((before,_after)) => {
-                                    println!("Trimmed by word back from {}", decoded.len());
-                                    Ok(Response::new(Body::from(before.to_string())))
-                                }
-                            }
-
-                        }
-                        Err(_) => {
-                            let mut not_found = Response::default();
-                            *not_found.status_mut() = StatusCode::NOT_FOUND;
-                            Ok(not_found)}
-                    }
-                }
-                _ => {
+                None => {
                     let mut not_found = Response::default();
                     *not_found.status_mut() = StatusCode::NOT_FOUND;
                     Ok(not_found)
                 }
             }
         }
-
+        (&Method::POST, path) if path.starts_with("/trimw/") => {
+            let len = path.strip_prefix("/trimw/").unwrap().parse::<usize>().ok();
+            match len {
+                Some(len) => {
+                    let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
+                    let body_string = String::from_utf8(body_bytes.to_vec())?;
+                    let encoded = tokenizer.encode(body_string, false)?;
+                    let mut trimmed = encoded.get_ids().to_vec();
+                    trimmed.truncate(len);
+                    let decoded = tokenizer.decode(trimmed, true).unwrap();
+                    match decoded.rsplit_once(" ") {
+                        None => {
+                            Ok(Response::new(Body::from(decoded)))
+                        }
+                        Some((before, _after)) => {
+                            Ok(Response::new(Body::from(before.to_string())))
+                        }
+                    }
+                }
+                None => {
+                    let mut not_found = Response::default();
+                    *not_found.status_mut() = StatusCode::NOT_FOUND;
+                    Ok(not_found)
+                }
+            }
+        }
         _ => {
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -80,14 +71,14 @@ async fn tokenize(tokenizer: Arc<Tokenizer>, req: Request<Body>) -> Result<Respo
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let server_details = env::var("SERVER_ADDR").unwrap_or("0.0.0.0:30000".to_string());
-    let pretrained = env::var("PRETRAINED").unwrap_or("gpt2".to_string());
+    let server_details = env::var("SERVER_ADDR").unwrap_or_else(|_| "0.0.0.0:30000".to_string());
+    let pretrained = env::var("PRETRAINED").unwrap_or_else(|_| "gpt2".to_string());
 
-    let addr: SocketAddr = server_details
-        .parse()
-        .expect("Unable to parse socket address");
+    let addr: SocketAddr = server_details.parse().expect("Unable to parse socket address");
 
-    let tokenizer = Tokenizer::from_pretrained(pretrained, None)?;
+    let tokenizer = Tokenizer::from_pretrained(pretrained, None)
+        .expect("Unable to initialize tokenizer");
+
     let tokenizer = Arc::new(tokenizer);
 
     let service = make_service_fn(|_| {
@@ -99,6 +90,7 @@ async fn main() -> Result<()> {
             }))
         }
     });
+
     let server = Server::bind(&addr).serve(service);
     println!("Listening on http://{}", addr);
     server.await?;
